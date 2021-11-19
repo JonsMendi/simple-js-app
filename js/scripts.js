0e3965bdb1682efe6eb8724cde7aa39998b65a55
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2021-11-19 11:54:34 -0100

    Updated scripts.js with new Variable
    If applied, this commit will have updated the scripts.js file with a new variable recipeList that contains 3 new objects, each one with 3 different keys (name, type and difficulty) and each keys with their own values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello world');
+let recipeList = [
+  {name: 'Meat and Poultry', type:['Beef', 'Duck', 'Lamb'], difficulty: [1, 2, 3]},
+  {name: 'Fish and Seafood', type:['Cod', 'Salmon', 'Prawns'], difficulty: [1, 2, 3]},
+  {name: 'Diets', type:['Vegetarian', 'Vegan', 'Pescatarian'], difficulty: [1, 2, 3]}
+];
 
-let favoriteFood = 'Everything with Truffle';
-document.write(favoriteFood);
+console.log(recipeList[1].type[1]);
+document.write(recipeList[1].difficulty[2]);
