30bde7aa90710e058cb8b8a9f56c6dcf7ac28878
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2021-11-23 17:06:12 -0100

    Updated scripts.js with new Loop and Condition
    If applied, this commit will have updated the scripts.js page with a new Loop that checks the difficulty of each object in the recipeList.
    This information is then printed in the website

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 let recipeList = [
-  {name: 'Meat and Poultry', type:['Beef', 'Duck', 'Lamb'], difficulty: [1, 2, 3]},
-  {name: 'Fish and Seafood', type:['Cod', 'Salmon', 'Prawns'], difficulty: [1, 2, 3]},
-  {name: 'Diets', type:['Vegetarian', 'Vegan', 'Pescatarian'], difficulty: [1, 2, 3]}
+  {name: 'Meat and Poultry', type:['Beef', 'Duck', 'Lamb'], difficulty: [10], height: [1, 2]},
+  {name: 'Fish and Seafood', type:['Cod', 'Salmon', 'Prawns'], difficulty: [7], height: [1, 2]},
+  {name: 'Diets', type:['Vegetarian', 'Vegan', 'Pescatarian'], difficulty: [4], height: [1, 2]}
 ];
 
-console.log(recipeList[1].type[1]);
-document.write(recipeList[1].difficulty[2]);
+for (let i = 0; i < recipeList.length; i++){ // here the initialization 'walk' start on 0 (i=0); condition for the 'direction' until the 'end' of the array(that's why 'length'; action to 'walk' until the end of the array in the 'direction' from '0'(i) to the 'end')
+  if(recipeList[i].difficulty < 5 && recipeList[i].difficulty > 0){ // in this section i'm saying to print all objects smaller then 5 and greater then 0.
+    document.write(recipeList[i].name + ' (difficulty:) ' + recipeList[i].difficulty + '<br>') // recipeList[i].name will print the name + '(difficulty)' will print the string + recipeList[i].difficulty will print the value of difficulty data.
+  }else if(recipeList[i].difficulty < 8 && recipeList[i].difficulty > 4){ // in this section i'm saying to print all objects smaller then 8 and greater then 4.
+    document.write(recipeList[i].name + ' (difficulty:) ' + recipeList[i].difficulty + '<br>')
+  }else{ // in this section i'm saying to print everything else
+    document.write(recipeList[i].name + ' (difficulty:) ' + recipeList[i].difficulty + ' - Sweeet, this one is hard! ' + '<br>')
+  }
+}
