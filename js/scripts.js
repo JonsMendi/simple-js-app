787d72d849c033d7f6bc4f903101e3760dc05fba
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2021-11-30 14:29:20 -0100

    Added an IIFE in recipeList with a two new internal functions and an updated forEach() function on the file scripts.js
    If applied, this commit will have added the IIFE (Immediately Invoked Function Expression) a Functional Programming Paradigm
    that organizes and protecting the global variables from being affected.
    In order of that, a new variable named recipeRepository as been created with an attached function that contains the recipeList.
    The functions 'add' and 'getAll' have been created inside of the new recipeRepository.
    The function 'add' have an typeof that just allow object input.
    A forEach() function was insert to update the previous 'for' Loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,43 @@
-let recipeList = [
-  {name: 'Meat and Poultry', type:['Beef', 'Duck', 'Lamb'], difficulty: [10], height: [1, 2]},
-  {name: 'Fish and Seafood', type:['Cod', 'Salmon', 'Prawns'], difficulty: [7], height: [1, 2]},
-  {name: 'Diets', type:['Vegetarian', 'Vegan', 'Pescatarian'], difficulty: [4], height: [1, 2]}
-];
-
-for (let i = 0; i < recipeList.length; i++){ // here the initialization 'walk' start on 0 (i=0); condition for the 'direction' until the 'end' of the array(that's why 'length'; action to 'walk' until the end of the array in the 'direction' from '0'(i) to the 'end')
-  if(recipeList[i].difficulty < 5 && recipeList[i].difficulty > 0){ // in this section i'm saying to print all objects smaller then 5 and greater then 0.
-    document.write(recipeList[i].name + ' (difficulty:) ' + recipeList[i].difficulty + '<br>') // recipeList[i].name will print the name + '(difficulty)' will print the string + recipeList[i].difficulty will print the value of difficulty data.
-  }else if(recipeList[i].difficulty < 8 && recipeList[i].difficulty > 4){ // in this section i'm saying to print all objects smaller then 8 and greater then 4.
-    document.write(recipeList[i].name + ' (difficulty:) ' + recipeList[i].difficulty + '<br>')
-  }else{ // in this section i'm saying to print everything else
-    document.write(recipeList[i].name + ' (difficulty:) ' + recipeList[i].difficulty + ' - Sweeet, this one is hard! ' + '<br>')
+/*Under we Start with the IIFE(Immediately Invoked Function Expression) a Functional Programming Paradigm
+That helps organizing and protecting the global variables from being affected*/
+let recipeRepository = (function(){
+  let recipeList = [
+    {name: 'Meat and Poultry', type:['Beef', 'Duck', 'Lamb'], difficulty: [10], height: [1, 2]},
+    {name: 'Fish and Seafood', type:['Cod', 'Salmon', 'Prawns'], difficulty: [7], height: [1, 2]},
+    {name: 'Diets', type:['Vegetarian', 'Vegan', 'Pescatarian'], difficulty: [4], height: [1, 2]}
+  ]
+  /*the function under 'add' have a 'typeof' to make sure that the 'typeof' value input is an object, if not is rejected*/
+  function add (recipe) {
+    if(typeof recipe === 'object') {
+      recipeList.push(recipe);
+    }else{
+      document.write('The values are not a object')
+    }
   }
-}
+
+  function getAll () {
+    return recipeList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+/*Now, with IIFE the only way possible to access the recipeRepository is through the 2 functions 'add' and 'getAll'*/
+
+
+/*recipeListLoop was a variable created to access the IIFE through the getAll function*/
+let recipeListLoop = recipeRepository.getAll();
+
+
+/*'froEach' function is a more effective already set up function to substitute the 'for' Loop*/
+recipeListLoop.forEach(function(recipe){
+  if(recipe.difficulty < 5 && recipe.difficulty > 0){ // in this section i'm saying to print all objects smaller then 5 and greater then 0.
+    document.write(recipe.name + ' (difficulty:) ' + recipe.difficulty + '<br>') // recipeList[i].name will print the name + '(difficulty)' will print the string + recipeList[i].difficulty will print the value of difficulty data.
+  }else if(recipe.difficulty < 8 && recipe.difficulty > 4){ // in this section i'm saying to print all objects smaller then 8 and greater then 4.
+    document.write(recipe.name + ' (difficulty:) ' + recipe.difficulty + '<br>')
+  }else{ // in this section i'm saying to print everything else
+    document.write(recipe.name + ' (difficulty:) ' + recipe.difficulty + ' - Sweeet, this one is hard! ' + '<br>')
+  }
+});
