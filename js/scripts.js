5b4216675b28ff6479a096b4479e7fc7ccf52cbd
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2021-12-05 16:01:45 +0000

    Added new 'addListItem' and 'showDetails' functions that generate and create a new Unordered List and is content. New typeof added in 'add' function and 'forEach' function improved.
    If applied, this commit will have added a new 'addListItem' that creates a new recipeUnordered List, is own 'li' and a new 'button' that contains an eventListener with 'click' that will generate the new recipeList in the web page.
    Another new 'showDetails' function have been created to be part of the eventListener function helping to generate further information.
    The 'forEach()' function have been updated creating now a Loop in the function 'addListItem' that generate the recipeList.
    
    Old information as 'filter()', 'check()' function or the previous conditions that we gave to the previous Loop being generated by the 'forEach()' function have been saved inside of /**/ in the end of the page

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,32 @@ let recipeRepository = (function(){
   ]
   /*the function under 'add' have a 'typeof' to make sure that the 'typeof' value input is an object, if not is rejected*/
   function add (recipe) {
-    if(typeof recipe === 'object') {
-      recipeList.push(recipe);
+    if(typeof recipe === 'object' &&
+        'name' in recipe &&
+        'type' in recipe &&
+        'difficulty' in recipe){
+        recipeList.push(recipe);
     }else{
-      document.write('The values are not a object')
+      document.write('The recipe is not correct');
     }
+  }
+
+  function addListItem (recipe) {
+    let recipeUnorderedList = document.querySelector('.recipe-list');
+    let recipeListItem = document.createElement('li');
+
+    let button = document.createElement('button');
+    button.innerText = recipe.name;
+    button.classList.add('button-style');
+
+    recipeListItem.appendChild(button);
+    recipeUnorderedList.appendChild(recipeListItem);
+    /*Under we are creating an Event Listener opening on click using the 'showDetails' function that we create to show/open the recipeList*/
+    button.addEventListener('click', showDetails(recipe));
+  }
+  /*Under we create a 'showDetails' function to open our recipeList in the Event Listener*/
+  function showDetails (recipe) {
+    console.log(recipeList);
   }
 
   function getAll () {
@@ -21,23 +42,57 @@ let recipeRepository = (function(){
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 /*Now, with IIFE the only way possible to access the recipeRepository is through the 2 functions 'add' and 'getAll'*/
 
 
-/*recipeListLoop was a variable created to access the IIFE through the getAll function*/
-let recipeListLoop = recipeRepository.getAll();
-
-
-/*'froEach' function is a more effective already set up function to substitute the 'for' Loop*/
-recipeListLoop.forEach(function(recipe){
-  if(recipe.difficulty < 5 && recipe.difficulty > 0){ // in this section i'm saying to print all objects smaller then 5 and greater then 0.
-    document.write(recipe.name + ' (difficulty:) ' + recipe.difficulty + '<br>') // recipeList[i].name will print the name + '(difficulty)' will print the string + recipeList[i].difficulty will print the value of difficulty data.
-  }else if(recipe.difficulty < 8 && recipe.difficulty > 4){ // in this section i'm saying to print all objects smaller then 8 and greater then 4.
-    document.write(recipe.name + ' (difficulty:) ' + recipe.difficulty + '<br>')
-  }else{ // in this section i'm saying to print everything else
-    document.write(recipe.name + ' (difficulty:) ' + recipe.difficulty + ' - Sweeet, this one is hard! ' + '<br>')
-  }
+/*'froEach()' function under. Is a more effective already set up function to substitute the 'for' Loop*/
+recipeRepository.getAll().forEach(function(recipe){
+  recipeRepository.addListItem(recipe);
 });
+
+
+
+
+
+
+
+
+
+
+/*---*/
+/*let recipeListFilter = recipeRepository.getAll();*/
+/*'filter()' function under. It helps searching*/
+/*
+recipeListFilter.filter(function(recipe){
+    document.write('<p>' + recipe.name + '</p>')
+    document.write(recipe.difficulty < 5)
+});
+*/
+
+
+/*---*/
+/*
+let validKeyNames = ['name', 'type', 'difficulty', 'height']
+
+function check (obejct) {
+  return validKeyNames.every({}.hasOwnProperty.bind(object));
+};
+
+document.write(recipeRepository.add(recipeRepository.check));
+*/
+
+/*---*/
+/*
+if(recipe.difficulty < 5 && recipe.difficulty > 0){ // in this section i'm saying to print all objects smaller then 5 and greater then 0.
+  document.write('<p>' + recipe.name + ' (difficulty:) ' + recipe.difficulty + '</p>') // recipeList[i].name will print the name + '(difficulty)' will print the string + recipeList[i].difficulty will print the value of difficulty data.
+}else if(recipe.difficulty < 8 && recipe.difficulty > 4){ // in this section i'm saying to print all objects smaller then 8 and greater then 4.
+  document.write('<p>' + recipe.name + ' (difficulty:) ' + recipe.difficulty + '</p>')
+}else{ // in this section i'm saying to print everything else
+  document.write('<p>' + recipe.name + ' (difficulty:) ' + recipe.difficulty + ' - Sweeet, this one is hard! ' + '</p>')
+}
+*/
